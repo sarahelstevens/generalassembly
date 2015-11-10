965c578d84aab9e6f1979be7e8412c592b50d04c
sarahelstevens <sarah.el.stevens@gmail.com>
2015-11-10 17:20:25 +1100

    CitiPix Assignment 6
    Week 7

---
+++ FEWD/Week Six/assignment-6_starter_code/starter_code/js/script.js
@@ -0,0 +1,36 @@
+
+$(document).ready(function(){
+	// create array of city names  
+	var cities = ['NYC','SF','LA','ATX','SYD'];
+	// create for loop to iterate through cities array
+	var text = '';
+	var i;
+	for (i = 0; i < cities.length; i++)
+	//add option elements to dropdown menu
+	{	var option = document.createElement('option');
+		option.text = cities[i];
+	//append city names to dropdown options
+		$('#city-type').append(option);
+	}
+});
+
+// 	determine input selected and create event on menu change
+	$('select').change(function(event){
+	event.preventDefault();
+	//create variable of user input	
+	var cities = $('#city-type').val();
+
+	//change background image depending on input value	
+if (cities == 'NYC'){
+		$('body').attr('class','nyc');	
+	} else if (cities == 'SF'){ 
+		$('body').attr('class','sf');
+	} else if (cities == 'LA'){ 
+		$('body').attr('class','la');
+	} else if (cities == 'ATX'){ 
+		$('body').attr('class','austin');
+	} else if (cities == 'SYD'){ 
+		$('body').attr('class','sydney');	
+}
+
+});

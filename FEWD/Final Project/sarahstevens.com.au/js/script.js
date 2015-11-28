0f1f91792b35f1bf2d7f6b1d307dd5dcde3a3a1e
sarahelstevens <sarah.el.stevens@gmail.com>
2015-11-28 14:17:19 +1100

    Final Project Week 9

--- FEWD/Final Project/sarahstevens.com.au/js/script.js
+++ FEWD/Final Project/sarahstevens.com.au/js/script.js
@@ -1,16 +1,34 @@
 //Push footer to bottom of page if page is shorter than window height
 
-$(document).ready(function(){
-
 var h = window.innerHeight;
 	body = $('section').height();
+	nav =$('nav').height();
+
+console.log(body);
 
 if (body < h){
-	$('section').css('height', h-250);
-} else if (body > h){
-	$('section').css('none');
+	$('section').css('height', h-600);
+} else if (body > h || body === h){
+	//do nothing
 }
 
+//Mobile Menu Slide Down
+
+function toggleMobileNav(){
+    $('nav, .mobile-nav').toggleClass('open');
+    $('.mobile-nav').text('Close').css('color', '#FDB2B5');
+    $('nav.open').css('height', h);
+
+    if ($('.mobile-nav').hasClass('open') === false) {
+    	$('.mobile-nav').text('Menu').css('color', '#333');
+    	$('nav').css('height', nav);
+    }
+
+  }
+
+$(document).ready(function(){
+  $('.mobile-nav').click(toggleMobileNav);
 });
 
-//Mobile Menu Slide Down
+
+//
